f24c6014386cd8bbb4d9f1cc1f219c3208451c53
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-03 12:56:01 -0600

    Updated props

--- src/components/Flex/Flex.js
+++ src/components/Flex/Flex.js
@@ -11,45 +11,68 @@ const Flex = (props) => {
 };
 
 Flex.propTypes = {
-  alignContent: PropTypes.oneOf([
+  justifyContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
     "start",
     "end",
+    "left",
+    "right",
     "center",
-    "between",
-    "around",
-    "stretch",
+    "space-between",
+    "space-around",
+    "space-evenly"
   ]),
   alignItems: PropTypes.oneOf([
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "start",
+    "end",
+    "self-start",
+    "self-end",
+    "center",
+    "baseline"
+  ]),
+  alignContent: PropTypes.oneOf([
+    "normal",
+    "flex-start",
+    "flex-end",
     "start",
     "end",
     "center",
-    "baseline",
-    "stretch",
+    "space-between",
+    "space-around",
+    "space-evenly",
+    "stretch"
   ]),
-
-  direction: PropTypes.oneOf(["row", "column", "row-responsive"]),
-
-  gap: PropTypes.number,
-
-  grow: PropTypes.number,
-
-  shrink: PropTypes.number,
-
-  order: PropTypes.number,
-
-  basis: PropTypes.number,
-
-  margin: PropTypes.oneOf([
-    "xxsmall",
-    "xsmall",
-    "small",
-    "medium",
-    "large",
-    "xlarge",
-    "xxlarge",
+  alignSelf: PropTypes.oneOf([
+    "auto",
+    "flex-start",
+    "flex-end",
+    "center",
+    "baseline",
+    "stretch"
+  ]),
+  direction: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse"
   ]),
   wrap: PropTypes.oneOf(["nowrap", "wrap", "wrap-reverse"]),
-
+  gap: PropTypes.oneOfType([
+    PropTypes.shape({
+      rowGap: PropTypes.string,
+      columnGap: PropTypes.string,
+    })
+  ]),
+  order: PropTypes.number,
+  grow: PropTypes.number,
+  shrink: PropTypes.number,
+  basis: PropTypes.string,
+  as: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default Flex;
--- src/components/Flex/presets/0-default.jsx
+++ src/components/Flex/presets/0-default.jsx
@@ -1,14 +1,20 @@
 import * as React from "react";
 import Flex from "../Flex";
-import Button from "../../Button/Button"
+import Button from "../../Button/Button";
 
 export default (
-  <Flex uxpId="flex-container" gap="2rem">
+  <Flex
+    uxpId="flex-container"
+    gap={{
+      rowGap: "5rem",
+      columnGap: "5rem",
+    }}
+  >
     <Flex uxpId="flex-1">
-    <Button  uxpId="button-1" label="Button 1" />
+      <Button uxpId="button-1" label="Button 1" />
     </Flex>
     <Flex uxpId="flex-2">
-    <Button  uxpId="button-2" label="Button 2" />
+      <Button uxpId="button-2" label="Button 2" />
     </Flex>
   </Flex>
 );
