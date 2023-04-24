52482655e267088b1ad813a861b5edcde718387b
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-24 13:08:34 -0600

    Updated Icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -12,5 +12,8 @@ export default function Icon({  icon }) {
 Icon.propTypes = {
   icon: PropTypes.oneOf([
     'ArrowDown',
+    'ArrowNext',
+    'ArrowPrevious',
+    'ArrowUp',
   ]),
 };
--- src/components/Icon/icons/index.js
+++ src/components/Icon/icons/index.js
@@ -1,5 +1,13 @@
 import ArrowDown from './arrow-down.svg';
+import ArrowNext from './arrow-next.svg';
+import ArrowPrevious from './arrow-previous.svg';
+import ArrowUp from './arrow-up.svg';
+
+
 
 export {
   ArrowDown,
+  ArrowNext,
+  ArrowPrevious,
+  ArrowUp,
 };
