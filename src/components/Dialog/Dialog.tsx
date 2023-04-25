d32749767c6f0ba7963b0150a35f2db7349d4612
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-25 12:51:22 -0600

    Dialog rendering in canvas

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -3,18 +3,47 @@ import { DxcDialog } from "@dxc-technology/halstack-react";
 import DialogPropsType from "@dxc-technology/halstack-react/dialog/types";
 
 export interface MergeDialogPropsType extends DialogPropsType {
-  open: boolean;
+  open?: boolean;
+  id?: string;
 }
+
+const Foo: React.FC<MergeDialogPropsType> = (props: MergeDialogPropsType) => {
+  return( <DxcDialog  id="jackDialog"{...props}><p>hello jack</p></DxcDialog>)
+}
+
 /**
  * @uxpindocurl https://developer.dxc.com/halstack/9/components/dialog/
- * @uxpinuseportal
+  * @uxpinuseportal
  */
 export default function Dialog(props: MergeDialogPropsType) {
   const [open, setOpen] = React.useState(props.open);
-  React.useEffect(() => setOpen(props.open), [props]);
+
+  React.useEffect(() => {
+    setOpen(props.open)
+  }, [props.open]);
+  
+  React.useLayoutEffect(() => {
+
+    if (document.getElementById("jack2")) {
+      alert("yes")
+      const container = document.getElementById('jack2');
+      let dialog = document.querySelector('[role="dialog"]')?.parentElement;
+      container!.appendChild(dialog!)
+    }
+
+  }, [props]);
+
+  
   return (
-    open && (
-      <DxcDialog {...props} />
-    )
-  );
+  open && (
+  <>
+    <div id="jack2">foo here</div>
+    <DxcDialog {...props}><p>hello jack0!</p></DxcDialog>
+    </>
+  )
+    
+    // open && (
+    //   Foo
+    // )
+  )
 }
