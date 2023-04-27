06007f211bf4561383e99a82232703ecbad01bcd
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-27 09:53:00 -0600

    changed children type

--- src/components/BulletedListItem/BulletedListItem.tsx
+++ src/components/BulletedListItem/BulletedListItem.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { DxcBulletedList } from "@dxc-technology/halstack-react";
 
 /**
  * @uxpindocurl https://developer.dxc.com/halstack/9/components/bulleted-list/
  */
 
-export default function BulletedListItem(props: { children: any }) {
+export default function BulletedListItem(props: { children: React.ReactNode }) {
   return (<div><DxcBulletedList.Item>{props.children}</DxcBulletedList.Item></div>)
 }
 
