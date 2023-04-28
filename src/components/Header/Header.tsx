67f241c45a9c980e885dcf9caacbf362fda18a80
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-04-28 14:13:09 -0600

    omit content prop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,9 @@ import HeaderPropsType from "@dxc-technology/halstack-react/header/types";
  * @uxpindocurl https://developer.dxc.com/halstack/9/components/header/
  */
 
-export interface MergeHeaderPropsType extends HeaderPropsType{
+export interface MergeHeaderPropsType extends Omit<HeaderPropsType, "content">{
   children: React.ReactNode;
+
 }
 /**
  * Clone DxcApplicationLayout.Header so that we can append a children prop, to be used as content
