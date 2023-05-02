d48edade09f8b6d263ef5410e75e311749f1fbc2
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-05-02 13:57:32 -0600

    Cleanup

--- src/components/QuickNav/QuickNav.tsx
+++ src/components/QuickNav/QuickNav.tsx
@@ -4,15 +4,10 @@ import Props from "@dxc-technology/halstack-react/quick-nav/types";
 /**
  * @uxpindocurl https://developer.dxc.com/halstack/9/components/quick-nav/
  */
-/**
- * @uxpinwrappers
- * SkipContainerWrapper
- */
+
 export default function QuickNav(props: Props) {
   return (
-
       <DxcQuickNav {...props} />
-
   );
 }
 
