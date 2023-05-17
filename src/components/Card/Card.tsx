47e7bd190ba1a3fded78380336d020a3a54598e0
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-05-17 12:32:44 -0600

    card test

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,10 +3,10 @@ import { DxcCard as DxcCardUXP } from "@dxc-technology/halstack-react";
 import CardPropsType from "@dxc-technology/halstack-react/card/types";
 
 export interface MergeCardPropsType extends Omit<CardPropsType, "onClick">{
-  // onClick: ()=>null
+  onClick?: ()=>(void)
 }
 
 
 export default function DxcCard(props: MergeCardPropsType) {
-  return <div><DxcCardUXP {...props} /></div>;
+  return <div><DxcCardUXP onClick={undefined}>{props.children}</DxcCardUXP></div>;
 }
